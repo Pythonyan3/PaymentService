80c32a741fe2a6f50bf1d0e48a236a39ab04c6c3
pythonyan3 <vitalii.manoilo3@gmail.com>
2022-06-12 21:17:53 +0300

    :sparkles: add; add db migrations;

---
+++ migrations/000001_create_transaction_table.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS "transaction" (
+    id serial not null unique,
+    user_id integer not null,
+    user_email varchar(254) not null,
+    amount bigint not null CHECK (amount > 0),
+    currency char(3) not null CHECK (length(currency) = 3),
+    status varchar(8) not null default 'NEW' CHECK (status IN ('NEW', 'ERROR', 'SUCCESS', 'FAILED', 'CANCELED')),
+    created_at timestamp with time zone default now()::timestamptz,
+    updated_at timestamp with time zone default now()::timestamptz
+);
+
+COMMIT;
