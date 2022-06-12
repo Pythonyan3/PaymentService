c8b81c5677dd753853b97e2b3bebbe4f63ef0f43
pythonyan3 <vitalii.manoilo3@gmail.com>
2022-06-12 21:05:24 +0300

    :sparkles: feature; update app struct run method (add service port to config);

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,21 +1,87 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/Pythonyan3/payment-service/config"
+	"github.com/Pythonyan3/payment-service/internal/database"
+	"github.com/Pythonyan3/payment-service/internal/handlers"
+	"github.com/Pythonyan3/payment-service/internal/repositories"
+	"github.com/Pythonyan3/payment-service/internal/server"
+	"github.com/Pythonyan3/payment-service/internal/services"
+
+	"github.com/gorilla/mux"
 )
 
 type Application struct{}
 
 func (app *Application) Run() error {
 	// application entrypoint method, initialize whole things.
-	log.Println("Service is started!")
+	log.Println("Service is starting...")
+
+	// initialize all variables
+	var err error
+	var cfg *config.Config
+	var router *mux.Router
+	var postgresDB *database.PostgresDB
+	var httpServer *server.Server
+	// repositories
+	var transactionRepository *repositories.TransactionPostgresRepository
+	var userRepository *repositories.UserPostgresRepository
+	// services
+	var transactionService *services.TransactionService
+	var userService *services.UserService
+	//handlers
+	var userHandler *handlers.UserHandler
+	var transactionHandler *handlers.TransactionHandler
+
+	// initialize variables
+	// parse config (env variables)
+	cfg = config.GetConfig()
+
+	// create postgres DB connection
+	postgresDB, err = database.NewPostgresDB(cfg)
+	if err != nil {
+		return fmt.Errorf("NewPostgresDb failed: %w", err)
+	}
+
+	// create repositories
+	transactionRepository = repositories.NewTransactionPostgresRepository(postgresDB)
+	userRepository = repositories.NewUserPostgresRepository(postgresDB)
+
+	// create services
+	transactionService = services.NewTransactionService(transactionRepository)
+	userService = services.NewUserService(userRepository)
+
+	// create handler
+	transactionHandler = handlers.NewTransactionHandler(transactionService)
+	userHandler = handlers.NewUserHandler(userService)
+
+	router = mux.NewRouter().PathPrefix("/api").Subrouter()
+
+	// init routes
+	userHandler.InitRoutes(router)
+	transactionHandler.InitRoutes(router)
+
+	// create and starting server server
+	httpServer = server.NewServer(cfg.ServicePort, router)
+
+	go func() {
+		if err := httpServer.Run(); err != nil {
+			log.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
 
 	// waiting for Ctrl + C to exit application
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
+
+	log.Println("Disconnecting db...")
+	postgresDB.Close()
 
 	log.Println("Service is shutted down!")
 
