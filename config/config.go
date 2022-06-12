7e8c7b89538eca1e23e83ed6cf4d690616d96a28
pythonyan3 <vitalii.manoilo3@gmail.com>
2022-06-12 21:05:24 +0300

    :sparkles: feature; update app struct run method (add service port to config);

--- config/config.go
+++ config/config.go
@@ -13,12 +13,13 @@ var (
 )
 
 type Config struct {
-	DBHost     string `envconfig:"DB_HOST" required:"true"`
-	DBPort     string `envconfig:"DB_PORT" required:"true"`
-	DBUser     string `envconfig:"DB_USER" required:"true"`
-	DBPassword string `envconfig:"DB_PASSWORD" required:"true"`
-	DBName     string `envconfig:"DB_NAME" required:"true"`
-	DBSSLMode  string `envconfig:"DB_SSL_MODE" required:"true"`
+	ServicePort string `envconfig:"SERVICE_PORT" default:"8000"`
+	DBHost      string `envconfig:"DB_HOST" required:"true"`
+	DBPort      string `envconfig:"DB_PORT" required:"true"`
+	DBUser      string `envconfig:"DB_USER" required:"true"`
+	DBPassword  string `envconfig:"DB_PASSWORD" required:"true"`
+	DBName      string `envconfig:"DB_NAME" required:"true"`
+	DBSSLMode   string `envconfig:"DB_SSL_MODE" required:"true"`
 }
 
 func GetConfig() *Config {
