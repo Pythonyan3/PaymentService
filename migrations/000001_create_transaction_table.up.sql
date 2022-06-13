27938b9dca0425bd00ce7d7bea6bf71a40a5afe4
pythonyan3 <vitalii.manoilo3@gmail.com>
2022-06-13 09:41:24 +0300

    :ambulance: hotfix; remove transaction amount constraint;

--- migrations/000001_create_transaction_table.up.sql
+++ migrations/000001_create_transaction_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS "transaction" (
     id serial not null unique,
     user_id integer not null,
     user_email varchar(254) not null,
-    amount bigint not null CHECK (amount > 0),
+    amount bigint not null,
     currency char(3) not null CHECK (length(currency) = 3),
     status varchar(8) not null default 'NEW' CHECK (status IN ('NEW', 'ERROR', 'SUCCESS', 'FAILED', 'CANCELED')),
     created_at timestamp with time zone default now()::timestamptz,
